8a7c5e990c41552dfc0ce869daf45d1fb0b24e12
Vanessa <vanessa_bcarvalho@hotmail.com>
2026-02-23 17:28:25 -0300

    renderizar

---
+++ cartas-personagens-edfeec1395c2708f8b1b7b7370b6226658b976f3/cartas-personagens-edfeec1395c2708f8b1b7b7370b6226658b976f3/src/modules/personagem.js
@@ -0,0 +1,21 @@
+ export class Personagem{
+    nome
+    vida
+    mana
+    level
+    tipo
+    descricao
+
+    constructor(nome, level, tipo){
+        this.nome = nome
+        this.level = level
+        this.tipo = tipo
+    }
+
+    obterInsignia() {
+        if (this.level >= 5){
+            return `Implacavel ${this.tipo}`
+        }
+        return `${this.tipo} iniciante`
+    }
+}
